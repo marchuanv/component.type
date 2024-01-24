8bb88cffaa6161a611a2c020b66e86e42c180c1f
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2024-01-24 05:28:18 +0200

    refactor

--- specs/index.mjs
+++ specs/index.mjs
@@ -1,20 +1,18 @@
-import { Specs, fileURLToPath, join } from "../registry.mjs";
+import { Specs, TypeDefinition, fileURLToPath, join } from "../registry.mjs";
 import { Animal } from "./classes/animal.mjs";
 import { Dog } from "./classes/dog.mjs";
 import { Food } from "./classes/food.mjs";
 let currentDir = fileURLToPath(new URL('./', import.meta.url));
 const scriptsDirPath = join(currentDir, 'classes');
-// TypeDefinition.register([
-//     { scriptsDirPath, scriptFileName: 'animal.mjs', targetClass: Animal },
-//     { scriptsDirPath, scriptFileName: 'food.mjs', targetClass: Food },
-//     { scriptsDirPath, scriptFileName: 'dog.mjs', targetClass: Dog },
-// ]).then(() => {
-//     const specs = new Specs(60000, './');
-//     specs.run();
-// }).catch((error) => {
-//     console.error(error);
-// });
-const specs = new Specs(60000, './');
-specs.run();
+TypeDefinition.register([
+    { scriptsDirPath, scriptFileName: 'animal.mjs', targetClass: Animal },
+    { scriptsDirPath, scriptFileName: 'food.mjs', targetClass: Food },
+    { scriptsDirPath, scriptFileName: 'dog.mjs', targetClass: Dog },
+]).then(() => {
+    const specs = new Specs(60000, './');
+    specs.run();
+}).catch((error) => {
+    console.error(error);
+});
 export { Animal, Dog, Food };
 
