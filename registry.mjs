44a1a3726e648ea72ba442b65b9ce1e039111cdd
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2024-01-24 10:52:52 +0200

    refactor

--- registry.mjs
+++ registry.mjs
@@ -1,16 +1,16 @@
+import { walkDir } from 'utils';
+import { MemberParameter } from './lib/member.parameter.mjs';
 import { TypeDefinition } from './lib/type.definition.mjs';
-
-export { GUID, Specs, VERBOSE, addSchema, existsSync, extname, fileURLToPath, join, readFileSync, validateSchema, walkDir } from 'utils';
+export { GUID, Specs, VERBOSE, addSchema, existsSync, extname, fileURLToPath, join, readFileSync, validateSchema } from 'utils';
 export { ClassIntegrity } from './lib/class.integrity.mjs';
 export { ClassInterface } from './lib/class.interface.mjs';
 export { ClassInterfaceRegister } from './lib/class.interface.register.mjs';
 export { Container } from './lib/container.mjs';
 export { Member } from './lib/member.mjs';
-export { MemberParameter } from './lib/member.parameter.mjs';
 export { MethodMember } from './lib/method.member.mjs';
 export { PrimitiveType } from './lib/primitivetype.mjs';
 export { PropertyMember } from './lib/property.member.mjs';
 export { ReferenceType } from './lib/referencetype.mjs';
 export { Schema } from './lib/schema.mjs';
-export { TypeDefinition };
+export { MemberParameter, TypeDefinition, walkDir };
 
--- specs/index.mjs
+++ specs/index.mjs
@@ -1,18 +1,27 @@
-import { Specs, TypeDefinition, fileURLToPath, join } from "../registry.mjs";
+import { ClassInterfaceRegister, Specs, fileURLToPath, join } from "../registry.mjs";
 import { Animal } from "./classes/animal.mjs";
 import { Dog } from "./classes/dog.mjs";
 import { Food } from "./classes/food.mjs";
-let currentDir = fileURLToPath(new URL('./', import.meta.url));
-const scriptsDirPath = join(currentDir, 'classes');
-TypeDefinition.register([
-    { scriptsDirPath, scriptFileName: 'animal.mjs', targetClass: Animal },
-    { scriptsDirPath, scriptFileName: 'food.mjs', targetClass: Food },
-    { scriptsDirPath, scriptFileName: 'dog.mjs', targetClass: Dog },
-]).then(() => {
+(async () => {
+    const currentDir = fileURLToPath(new URL('./', import.meta.url));
+    const scriptsDirPath = join(currentDir, 'classes');
+    await ClassInterfaceRegister.register([{
+        scriptsDirPath,
+        scriptFileName: 'food.mjs',
+        targetClass: Food
+    }, {
+        scriptsDirPath,
+        scriptFileName: 'animal.mjs',
+        targetClass: Animal
+    }, {
+        scriptsDirPath,
+        scriptFileName: 'dog.mjs',
+        targetClass: Dog
+    }]);
+    await ClassInterfaceRegister.generate(Food);
+    await ClassInterfaceRegister.generate(Animal);
+    await ClassInterfaceRegister.generate(Dog);
     const specs = new Specs(60000, './');
     specs.run();
-}).catch((error) => {
-    console.error(error);
-});
+})().catch((err) => console.error(err));
 export { Animal, Dog, Food };
-
