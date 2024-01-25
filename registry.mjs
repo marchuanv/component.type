b9a2817609649959d0ec6adc868b14b34938e424
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2024-01-25 15:04:10 +0200

    refactor

--- registry.mjs
+++ registry.mjs
@@ -1,16 +1,4 @@
-import { walkDir } from 'utils';
-import { GenericType } from './lib/generic.type.mjs';
-import { MemberParameter } from './lib/member.parameter.mjs';
-import { TypeDefinition } from './lib/type.definition.mjs';
 export { GUID, Specs, VERBOSE, existsSync, extname, fileURLToPath, join, readFileSync, registerSchema, validateSchema } from 'utils';
-export { ClassIntegrity } from './lib/class.integrity.mjs';
-export { ClassInterface } from './lib/class.interface.mjs';
-export { ClassInterfaceRegister } from './lib/class.interface.register.mjs';
 export { Container } from './lib/container.mjs';
 export { CtorArgs, CtorArgsRegistry } from './lib/ctor.args.mjs';
-export { Member } from './lib/member.mjs';
-export { MethodMember } from './lib/method.member.mjs';
-export { PropertyMember } from './lib/property.member.mjs';
-export { Schema } from './lib/schema.mjs';
-export { GenericType, MemberParameter, TypeDefinition, walkDir };
 
