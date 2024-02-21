f5d2cf9b4ad1c4cf47201def69915ad444f1d426
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2024-02-21 09:13:00 +0200

    refactor

--- specs/namespace-spec.mjs
+++ specs/namespace-spec.mjs
@@ -1,22 +1,24 @@
 import { Namespace } from '../registry.mjs';
 describe('when creating a valid namespace', () => {
-    it('should have equality between two namespaces with the same names', () => {
-        const id = new Namespace('part1');
-        const id2 = new Namespace('part1');
+    it('should have equality between two of the same namespaces', () => {
+        const id = new Namespace('segment1');
+        const id2 = new Namespace('segment1');
         expect(id).toBeDefined();
         expect(id).not.toBeNull();
         expect(id2).toBeDefined();
         expect(id2).not.toBeNull();
         expect(id).toBe(id2);
+        expect(id).toEqual(id2);
     });
-    it('should not have equality between two namespaces with different names', () => {
-        const id = new Namespace('part2');
-        const id2 = new Namespace('part2');
+    fit('should not have equality between two different namespaces', () => {
+        const id = new Namespace('segment1.segment1');
+        const id2 = new Namespace('segment1.segment2');
         expect(id).toBeDefined();
         expect(id).not.toBeNull();
         expect(id2).toBeDefined();
         expect(id2).not.toBeNull();
         expect(id).not.toBe(id2);
+        expect(id).not.toEqual(id2);
     });
 });
 describe('when creating an invalid namespace given a namespace of "part.$##@.%#$%"', () => {
