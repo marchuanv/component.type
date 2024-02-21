c0ca939474c55923cfcfaa64aca08a98c8aeb2ad
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2024-02-21 14:25:59 +0200

    refactor

--- specs/type-spec.mjs
+++ specs/type-spec.mjs
@@ -12,3 +12,31 @@ describe('when creating types given the same namespaces', () => {
         expect(typeA).toEqual(typeB);
     });
 });
+describe(`when getting a type that does exist given the common namespace and ${String.name} class`, () => {
+    let type = null;
+    beforeAll(() => {
+        type = Type.get('common', String);
+    });
+    it('should return an instance of type', () => {
+        expect(type).toBeDefined();
+        expect(type).not.toBeNull();
+        expect(type).toBeInstanceOf(Type);
+    });
+});
+class UnknownClass { }
+fdescribe(`when getting a type that does not exist given the common namespace and ${UnknownClass.name} class`, () => {
+    let error = null;
+    beforeAll(() => {
+        try {
+            Type.get('common', UnknownClass);
+        } catch (err) {
+            error = err;
+        }
+    });
+    it('should raise an error', () => {
+        expect(error).toBeDefined();
+        expect(error).not.toBeNull();
+        expect(error).toBeInstanceOf(Error);
+        expect(error.message).toBe(`type not found: ${JSON.stringify({ namespace: 'common', Class: UnknownClass.name })}`);
+    });
+});
