8b3241777cbd955df70ad350ee10b8c714e27910
marchuanv <marchuanv@gmail.com>
2024-02-25 04:41:55 +0200

    refactor

--- specs/type-spec.mjs
+++ specs/type-spec.mjs
@@ -1,17 +1,48 @@
 import { Type } from '../registry.mjs';
 class ClassA { }
 class UnknownClass { }
-class TestType extends Type { }
+class TestType extends Type {
+    /**
+     * @param { Object } target
+    */
+    constructor(target) {
+        super(Type.namespace, target);
+    }
+}
+class TestTypeWrongNamespace extends Type {
+    /**
+     * @param { Object } target
+    */
+    constructor(target) {
+        super('awdawdawd', target);
+    }
+}
 describe('when creating types given the same namespaces', () => {
     it('should have equality', () => {
-        const typeA = new TestType('segment.segment1', ClassA);
-        const typeB = new TestType('segment.segment1', ClassA);
+        const typeA = new TestType(ClassA);
+        const typeB = new TestType(ClassA);
         expect(typeA).toBeDefined();
         expect(typeA).not.toBeNull();
         expect(typeB).toBeDefined();
         expect(typeB).not.toBeNull();
         expect(typeA).toBe(typeB);
         expect(typeA).toEqual(typeB);
+    });
+});
+describe('when creating types given the wrong namespace', () => {
+    let error = null;
+    beforeAll(() => {
+        try {
+            new TestTypeWrongNamespace(ClassA);    
+        } catch (err) {
+            error = err;
+        }
+    });
+    it(`should raise an error`, () => {
+        expect(error).toBeDefined();
+        expect(error).not.toBeNull();
+        expect(error).toBeInstanceOf(Error);
+        expect(error.message).toBe('namespace argument does not start with component.types.');
     });
 });
 describe(`when getting a type that does exist given a ${String.name}`, () => {
