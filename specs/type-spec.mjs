4f0b4e39d35be8b0ffaae5fb7416a54b662b5741
marchuanv <marchuanv@gmail.com>
2024-02-25 04:55:47 +0200

    refactor

--- specs/type-spec.mjs
+++ specs/type-spec.mjs
@@ -1,22 +1,7 @@
 import { Type } from '../registry.mjs';
 class ClassA { }
 class UnknownClass { }
-class TestType extends Type {
-    /**
-     * @param { Object } target
-    */
-    constructor(target) {
-        super(Type.namespace, target);
-    }
-}
-class TestTypeWrongNamespace extends Type {
-    /**
-     * @param { Object } target
-    */
-    constructor(target) {
-        super('awdawdawd', target);
-    }
-}
+class TestType extends Type {}
 describe('when creating types given the same namespaces', () => {
     it('should have equality', () => {
         const typeA = new TestType(ClassA);
@@ -27,22 +12,6 @@ describe('when creating types given the same namespaces', () => {
         expect(typeB).not.toBeNull();
         expect(typeA).toBe(typeB);
         expect(typeA).toEqual(typeB);
-    });
-});
-describe('when creating types given the wrong namespace', () => {
-    let error = null;
-    beforeAll(() => {
-        try {
-            new TestTypeWrongNamespace(ClassA);    
-        } catch (err) {
-            error = err;
-        }
-    });
-    it(`should raise an error`, () => {
-        expect(error).toBeDefined();
-        expect(error).not.toBeNull();
-        expect(error).toBeInstanceOf(Error);
-        expect(error.message).toBe('namespace argument does not start with component.types.');
     });
 });
 describe(`when getting a type that does exist given a ${String.name}`, () => {
