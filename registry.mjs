8027f3f52d23c7ac399587c2a5eb62dafe63f458
marchuanv <marchuanv@gmail.com>
2024-02-27 00:42:06 +0200

    refactor

--- registry.mjs
+++ registry.mjs
@@ -1,6 +1,7 @@
+import { PropertyRegEx } from './lib/property-regex.mjs';
 export { Namespace } from 'component.namespace';
 export { Specs } from 'component.specs';
 export { GUID } from 'component.uuid';
 export { Type } from './lib/type.mjs';
-export { PropertyRegEx } from './lib/property-regex.mjs';
+export { PropertyRegEx }
 
