b65d6e7ae0ead727c62aaaeeaa35a7db2507ea75
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2024-03-15 11:15:22 +0200

    refactor

--- specs/type-spec.mjs
+++ specs/type-spec.mjs
@@ -1,93 +1,18 @@
-import { Type } from '../registry.mjs';
-import { Animal, AnimalSimilar, AnimalUnknown, Dog } from './index.mjs';
-fdescribe(`when creating in instance of the ${Type.name} class given that the target is ${Animal.name}`, () => {
-    let animal = null;
-    beforeAll(() => {
-        animal = new Animal();
-    });
-    it(`should have ${Animal.name} as the type`, () => {
-        expect(animal).toBeDefined();
-        expect(animal).not.toBeNull();
-    });
-});
-describe(`when creating in instance of the ${Type.name} class given that the target is ${Dog.name} that extends the ${Animal.name} class`, () => {
-    let dog = null;
-    beforeAll(() => {
-        dog = new Dog();
-    });
-    it(`should have ${Dog.name} as the type`, () => {
+import { Animal, Dog, Food } from './index.mjs';
+describe(`when creating in instance of the ${Dog.name} class given that it extends the ${Animal.name} class`, () => {
+    it(`should have the ${Animal.name} and ${Dog.name} extended classes`, () => {
+        const dog = new Dog();
         expect(dog).toBeDefined();
         expect(dog).not.toBeNull();
-        expect(dog.metadata.type).toBe(Dog);
-        expect(dog.metadata.typeExtended).toContain(Animal);
-        expect(dog.metadata.typeExtended).toContain(Dog);
+        expect(dog.extended).toContain(Animal, `does not have the ${Animal.name} class.`);
+        expect(dog.extended).toContain(Dog, `does not have the ${Dog.name} class.`);
     });
-});
-describe(`when getting a string representation of the ${Animal.name} class.`, () => {
-    let animalStr = null;
-    beforeAll(() => {
-        const animal = new Animal();
-        animalStr = animal.toString();
-    });
-    it(`should return a string`, () => {
-        expect(animalStr).toBeDefined();
-        expect(animalStr).not.toBeNull();
-        expect(animalStr).toBe(JSON.stringify({ namespace: "component.types.metadata.animal", typeName: "Animal" }));
-    });
-});
-describe('when creating a type given that the type already exists but it is different', () => {
-    let error = null;
-    beforeAll(() => {
-        try {
-            new Animal();
-            new AnimalSimilar();
-        } catch (err) {
-            console.log(err);
-            error = err;
-        }
-    });
-    it('should raise an error', () => {
-        expect(error).toBeDefined();
-        expect(error).not.toBeNull();
-        expect(error).toBeInstanceOf(Error);
-        expect(error.message).toBe(`The Animal class already exists.`);
-    });
-});
-describe(`when getting a type that does exist given a ${String.name}`, () => {
-    let type = null;
-    beforeAll(() => {
-        type = Type.get(String.name);
-    });
-    it(`should return an instance of ${String.name}`, () => {
-        expect(type).toBeDefined();
-        expect(type).not.toBeNull();
-        expect(type).toBe(String);
-    });
-});
-describe(`when getting a type that does exist given a ${String.name}`, () => {
-    let exists = null;
-    beforeAll(() => {
-        exists = Type.has(String.name);
-    });
-    it(`should return true`, () => {
-        expect(exists).toBeDefined();
-        expect(exists).not.toBeNull();
-        expect(exists).toBeTrue();
-    });
-});
-describe(`when getting a type that does not exist given the common namespace and ${AnimalUnknown.name} class`, () => {
-    let error = null;
-    beforeAll(() => {
-        try {
-            Type.get(AnimalUnknown.name);
-        } catch (err) {
-            error = err;
-        }
-    });
-    it('should raise an error', () => {
-        expect(error).toBeDefined();
-        expect(error).not.toBeNull();
-        expect(error).toBeInstanceOf(Error);
-        expect(error.message).toBe(`${AnimalUnknown.name} was not found.`);
+    it(`should associate the ${Food.name} class with the ${Dog.name} class`, () => {
+        const dog = new Dog();
+        const food = new Food();
+        expect(dog).toBeDefined();
+        expect(dog).not.toBeNull();
+        dog.associate(food);
+        expect(dog.associations).toContain(Food);
     });
 });
