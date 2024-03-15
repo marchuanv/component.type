3a8a745d7741ad4f7aba9358d0bacef7a533e075
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2024-03-15 14:23:26 +0200

    refactor

--- registry.mjs
+++ registry.mjs
@@ -1,10 +1,11 @@
 import { Properties, PropertyOptions } from 'component.property';
 import { TypeOptions } from './lib/type.options.mjs';
 export { Namespace } from 'component.namespace';
-export { Reference } from 'component.reference';
+export { Reference, ReferenceOptions } from 'component.reference';
 export { Specs } from 'component.specs';
 export { GUID, Store } from 'component.uuid';
 export { Reflection } from 'utils';
+export { TypeAssociation } from './lib/type-association.mjs';
 export { Type } from './lib/type.mjs';
 export { Properties, PropertyOptions, TypeOptions };
 
--- specs/type-spec.mjs
+++ specs/type-spec.mjs
@@ -7,12 +7,19 @@ describe(`when creating in instance of the ${Dog.name} class given that it exten
         expect(dog.extended).toContain(Animal, `does not have the ${Animal.name} class.`);
         expect(dog.extended).toContain(Dog, `does not have the ${Dog.name} class.`);
     });
-    it(`should associate the ${Food.name} class with the ${Dog.name} class`, () => {
+    it(`should associate ${Food.name} type with ${Dog.name} type`, () => {
         const dog = new Dog();
         const food = new Food();
         expect(dog).toBeDefined();
         expect(dog).not.toBeNull();
         dog.associate(food);
+        expect(dog.associations).toContain(Food);
+    });
+    it(`should associate the ${Food.name} class with the ${Dog.name} class`, () => {
+        const dog = new Dog();
+        expect(dog).toBeDefined();
+        expect(dog).not.toBeNull();
+        dog.associate(Food);
         expect(dog.associations).toContain(Food);
     });
 });
