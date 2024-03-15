1c612b4ae5a981adda9dd49f0be9f38ce912fabd
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2024-03-15 23:11:11 +0200

    refactor

--- registry.mjs
+++ registry.mjs
@@ -1,11 +1,8 @@
-import { Properties, PropertyOptions } from 'component.property';
-import { TypeOptions } from './lib/type.options.mjs';
-export { Namespace } from 'component.namespace';
-export { Reference, ReferenceOptions } from 'component.reference';
+export { Properties, PropertiesContext } from 'component.property';
+export { Reference, ReferenceContext, ReferenceId } from 'component.reference';
 export { Specs } from 'component.specs';
 export { GUID, Store } from 'component.uuid';
 export { Reflection } from 'utils';
 export { TypeAssociation } from './lib/type-association.mjs';
+export { TypeRegistry } from './lib/type-registry.mjs';
 export { Type } from './lib/type.mjs';
-export { Properties, PropertyOptions, TypeOptions };
-
